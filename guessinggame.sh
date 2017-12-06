9c879877549bd4a5b9bfd096423b308d42a9036b
Christopher Reece <mrkita.reece@gmail.com>
2017-12-06 12:14:09 +0900

    working on random strings from array

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,33 +1,49 @@
-# get number of files in current directory
-local num_files=$(ls -1 | wc -l)
-
-# welcome the user
-  displayText 'Welcome to G.G. otherwise known as the Guessing Game'
-  pause "Press enter to continue"
-  displayText "How many files do you think are in the current working directory? "
-  read guess
-  while (( guess != num_files )); do
-    if (( guess < num_files )); then
-      displayText "Not quite. Guess higher."
-      read guess
-    elif (( guess > num_files )); then
-      displayText "Too high. Try again"
-      read guess
-    fi
-  done
-
-echo $guess
-# prompt the user to guess the number of files
-
 # display text like in Pokemon :D
 function displayText {
   text=$1
   for (( i=0; i <${#text}; i++ )); do
     echo -n "${text:$i:1}"
-    sleep 0.05
+    sleep 0.03
   done
+  echo -e '\n'
 }
 
 function pause() {
   read -p "$*"
 }
+
+# get number of files in current directory
+num_files=$(ls -1 | wc -l)
+
+higherText[0]="Not quite. Guess higher."
+higherText[1]="A little higher."
+higherText[2]="Getting there... but still too low."
+higherText[3]="Try guessing higher"
+
+lowerText[0]="Your guess is in the sky"
+lowerText[1]="Try guessing lower"
+lowerText[2]="A lower guess might do the trick"
+lowerText[3]="Guess something lower"
+
+# welcome the user
+displayText "Welcome to G.G. otherwise known as the Guessing Game "
+pause "Press enter to continue"
+displayText "How many files do you think are in the current working directory? "
+read guess
+
+while (( guess != num_files )); do
+  rand=$(( (RANDOM % 4) + 1))
+  goHigherText=${higherText[$rand]} 
+  goLowerText=${higherText[$rand]} 
+
+  if (( guess < num_files )); then
+    echo $goHigherText | displayText
+    read guess
+  elif (( guess > num_files )); then
+    displayText $goLowerText
+    read guess
+  fi
+done
+
+displayText "Correct! there are $num_files in this directory 🎉🎉🎉🎉🎉"
+
